24cfbf03e9aac87e826883cc8ed68ac798c506e7
bubble <bubble@bubbledeMac-mini.local>
2026-08-11 18:51:42 +0800

    feat(deployment): register Kubernetes installer

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -113,6 +113,9 @@ func Run(info buildinfo.Info) error {
 	if err := deploymentCatalog.Register(deploymentcatalog.NewAuroraInstaller(releaseResolver)); err != nil {
 		return fmt.Errorf("register Aurora installer: %w", err)
 	}
+	if err := deploymentCatalog.Register(deploymentcatalog.NewKubernetesInstaller()); err != nil {
+		return fmt.Errorf("register Kubernetes installer: %w", err)
+	}
 	deploymentService := deployment.NewService(deploymentRepo, deploymentCatalog, deploymentCipher, assetService.Get, auditRepo, deploymentEvents, time.Now)
 	workerCipher := deploymentCipher
 	if workerCipher == nil {
